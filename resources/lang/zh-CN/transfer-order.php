2af8c42ef8e31399a9f34fce3dc10cbe9c8681aa
JinHui Xu <591196507@qq.com>
2025-12-10 00:19:09 +0800

    feat

--- resources/lang/zh-CN/transfer-order.php
+++ resources/lang/zh-CN/transfer-order.php
@@ -19,7 +19,7 @@ return [
     'fields' => [
         'order_no' => '单号',
         'created_at' => '业务日期',
-        'review_status' => '审核状态',
+        'review_status_str' => '审核状态',
         'out_position.name' => '调出仓库',
         'in_position.name' => '调入仓库',
         'user.name' => '创建人',
