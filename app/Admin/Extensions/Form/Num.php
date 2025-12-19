d63c783dbb1f0b6ffa4432b15a23162a6052a82e
JinHui Xu <591196507@qq.com>
2025-12-19 15:36:37 +0800

    feat

--- app/Admin/Extensions/Form/Num.php
+++ app/Admin/Extensions/Form/Num.php
@@ -14,14 +14,49 @@
 
 namespace App\Admin\Extensions\Form;
 
+use Dcat\Admin\Admin;
 use Dcat\Admin\Form\Field;
 
 class Num extends Field\Number
 {
+    protected function addScript()
+    {
+        $this->script = <<<JS
+(function () {
+    var selector = '{$this->getElementClassSelector()}';
+
+    $(selector + ':not(.initialized)')
+        .addClass('initialized')
+        .bootstrapNumber({
+            upClass: 'primary',
+            downClass: 'white',
+            center: true
+        });
+
+    // 数量一般较大，隐藏 +/- 按钮，避免占用输入宽度
+    $(selector).each(function () {
+        var \$input = $(this);
+        if (!\$input.hasClass('num-no-stepper')) {
+            return;
+        }
+
+        \$input.closest('.input-group').find('.input-group-btn').remove();
+    });
+})();
+JS;
+    }
+
     public function render()
     {
         $this->min(0);
-        $this->defaultAttribute('style', 'width: 4em');
+        $this->addElementClass('num-no-stepper');
+        $this->defaultAttribute('style', 'width: 12em;flex:none');
+
+        Admin::style(<<<'CSS'
+.number-group input.num-no-stepper {
+    text-align: left !important;
+}
+CSS);
 
         return parent::render();
     }
