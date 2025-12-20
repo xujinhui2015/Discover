bb80254c6db4bafc2c280efa368d3ba3f6f2eb4b
JinHui Xu <591196507@qq.com>
2025-12-20 23:05:52 +0800

    feat

--- public/vendors/dcat-admin/dcat/plugins/number-input/bootstrap-number-input.js
+++ public/vendors/dcat-admin/dcat/plugins/number-input/bootstrap-number-input.js
@@ -62,7 +62,7 @@
             // remove spins from original
             clone.prop('type', 'text').keydown(function (e) {
                 if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
-					(e.keyCode == 65 && e.ctrlKey === true) ||
+					((e.keyCode == 65 || e.keyCode == 86) && (e.ctrlKey === true || e.metaKey === true)) ||
 					(e.keyCode >= 35 && e.keyCode <= 39)) {
                     return;
                 }
