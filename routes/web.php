31ef89e004f87740e23a466b88a893796a59e989
JinHui Xu <591196507@qq.com>
2025-12-26 09:55:41 +0800

    feat

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+Route::get('/', function () {
+    return redirect('/admin');
+});
