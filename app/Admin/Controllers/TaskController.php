0a23feba75aa51da092c24845afac04ecc855e2f
JinHui Xu <591196507@qq.com>
2026-05-07 14:09:57 +0800

    feat

--- app/Admin/Controllers/TaskController.php
+++ app/Admin/Controllers/TaskController.php
@@ -114,6 +114,12 @@ class TaskController extends AdminController
                 }, '物料信息')->placeholder('物料名称，拼音码，编号')->width(3);
                 $filter->like('order_no')->width(3);
                 $filter->in('status', '单据状态')->multipleSelect(TaskModel::STATUS)->width(3);
+                $filter->where('product_type', function (Builder $query) {
+                    $types = (array) $this->getValue();
+                    $query->whereHasIn('sku.product', function (Builder $query) use ($types) {
+                        $query->whereIn('type', $types);
+                    });
+                }, '物料分类')->multipleSelect(ProductModel::TYPE)->width(3);
 //                $filter->group('plan_num', function ($group) {
 //                    $group->gt('大于');
 //                    $group->lt('小于');
